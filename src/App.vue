fb73f22d4f8a52fbd3d59c324c88327670c4b544
jiahao <1332204564@qq.com>
2018-02-05 18:16:04 +0800

    修复底部导航

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
   <div id="app">
     
      <router-view/>
+		
+     <!--根部-->
+		<foot></foot>
   </div>
 </template>
 
 <script>
+	import foot from '@/components/foot.vue';
 export default {
-  name: 'App'
+  name: 'App',
+  components:{foot}
 }
 </script>
 
