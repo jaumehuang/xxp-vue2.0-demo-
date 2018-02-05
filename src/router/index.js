493f618ce969c9cb8bf8b461b76ece2713028228
jiahao <1332204564@qq.com>
2018-02-06 01:10:09 +0800

    增加详情页

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 //import banner from '@/components/banner'
-import home from '@/view/home'
+import home from '../view/home'
 import classify from '@/view/classify'
 import cart from '@/view/cart'
 import person from '@/view/person'
+import detail from '@/view/detail'
+import pageView from '../view/pageView'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    
-    {
-      path: '/',
-      redirect: '/pages/'
-    },
-    {
-    	 path: '/pages',
-       component: home,
-       name:"home"
-    },
-     {
-    	 path: 'home',
-       component: home,
-       name:"home"
-    },
-    {
-    	 path: 'classify',
-       component: classify,
-       name:"classify"
-    },
-    {
-    	 path: 'cart',
-       component: cart,
-       name:"cart"
-    },
-    {
-     	 path: 'person',
-       component: person,
-       name:"person"
-    }
+	routes: [
 
-  ]
+		{
+			path: '/',
+			redirect: '/pages/'
+		},
+		{
+			path: '/pages',
+			component: pageView,
+			children: [
+
+				{
+					path: '',
+					redirect: '/pages/'
+				},
+				{
+					path: 'home',
+					component: home,
+					name: "home"
+				},
+				{
+					path: 'classify',
+					component: classify,
+					name: "classify"
+				},
+				{
+					path: 'cart',
+					component: cart,
+					name: "cart"
+				},
+				{
+					path: 'person',
+					component: person,
+					name: "person"
+				},
+			]
+		},
+
+		{
+			path: 'detail',
+			name: 'detail',
+			component: detail
+		}
+
+	]
 })
